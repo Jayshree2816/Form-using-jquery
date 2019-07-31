d675cc0e1948885b11e01b92ac9db59c42a876c7
Jayshree2816 <sonawanejayshree321@gmail.com>
2019-08-01 01:30:31 +0530

    added new form structure and js

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
      
+// $(document).ready(function(){
+//     $("#submitBtn").on("click", function(e){
+//         var fname = $("#firstName").val();
+//         var lname = $("#lastName").val();
+//         var contactNumber = $("#contact").val();
+
+//         if(fname==''||lname==''||contactNumber=='')
+//             {
+//                 alert("Please Fill All Fields");
+//                 // e.preventDefault();
+//             } 
+//             else {
+//                 // $("#t1").html(fname)
+//                 // $("#t2").html(lname)
+//                 // $("#t3").html(contactNumber)
+//                 alert("Form submitted")
+//                 e.preventDefault();
+//             }
+//         })
+//     })
+
+
 $(document).ready(function(){
-    $("#submitBtn").on("click", function(e){
+    $("#add").click(function(){
+        $("form").show()
+    })
+    $("#submit").on("click", function(e) {
         var fname = $("#firstName").val();
         var lname = $("#lastName").val();
         var contactNumber = $("#contact").val();
@@ -8,21 +33,17 @@ $(document).ready(function(){
         if(fname==''||lname==''||contactNumber=='')
             {
                 alert("Please Fill All Fields");
-                // e.preventDefault();
-            } 
-            else {
-                // $("#t1").html(fname)
-                // $("#t2").html(lname)
-                // $("#t3").html(contactNumber)
-                alert("Form submitted")
                 e.preventDefault();
             }
-            // $("#modal").on('hide.bs.modal', function(e){
-            //     var contentArea = $(this).find('#modalContent');
-            //     contentArea.html('');
-            // });
+            else {
+                var st = "";
+                $("#myForm input[type=text],input[type=tel],select").each(function(){
+                st = st+ '<td>'+$(this).val()+'</td>' ;
+                $(this).val('');
+            })
+            $('#details').append('<tr>'+st+'</tr>').append('<button>'+ 'Edit' + '</button>');
+        }
     })
 })
-
 
 
